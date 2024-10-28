5a7118d7706058422808a3b45a8e53cfd4200dc2
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2024-10-28 17:25:37 +0200

    scss

--- js/script.js
+++ js/script.js
@@ -15,24 +15,6 @@
     const modalBackdrop = document.getElementById('modalBackdrop');
   })();
 
-  function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('element-show');
-      } else {
-        change.target.classList.remove('element-show');
-      }
-    });
-  }
-  let options = { threshold: [0.4] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elementss = document.querySelectorAll('.container');
-  for (let elm of elementss) {
-    observer.observe(elm);
-  }
-
-
-
 
 
 
